b8d0b7012f51966fae910872a0e5124f0032e692
ouadeb12 <sibiri.ouisiga@gmail.com>
2026-04-08 15:52:30 +0000

    Create CosoburkinsDashboard component

---
+++ src/components/CosoburkinsDashboard.js
@@ -0,0 +1,12 @@
+// Cosoburkins Dashboard Component
+import React from 'react';
+
+const CosoburkinsDashboard = () => {
+    return (
+        <div>
+            <h1>Welcome to the COSO Burkina Dashboard!</h1>
+        </div>
+    );
+};
+
+export default CosoburkinsDashboard;
